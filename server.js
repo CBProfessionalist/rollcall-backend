39d82f47b9f366bab8ff1bbfd05475247c378b43
CBProfessionalist <cheifbabang123@gmail.com>
2026-02-17 11:40:11 -0800

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:8000', 'https://rollcall-frontend.vercel.app'],
+    origin: ['http://localhost:8000', 'https://rollcall-frontend1.vercel.app'],
     credentials: true
 }));
 
